b56a94b837d4972d55b37576e8a78aba328fb912
Paulo Rocha <pcrochadev@gmail.com>
2025-09-05 21:26:19 -0300

    feat(back): check if game exists

---
+++ src/app/api/game/check/[userId]/route.ts
@@ -0,0 +1,37 @@
+import { type NextRequest, NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+
+interface Props {
+  params: Promise<{ userId: string }>;
+}
+
+export async function GET(_: NextRequest, props: Props) {
+  try {
+    const { userId } = await props.params;
+    if (!userId)
+      return NextResponse.json(
+        { error: "userId é obrigatório" },
+        { status: 400 }
+      );
+
+    const client = await clientPromise;
+    const db = client.db("data");
+
+    const game = await db.collection("games").findOne({
+      userId,
+      state: "playing",
+    });
+
+    if (!game)
+      return NextResponse.json(
+        { error: "Nenhum jogo encontrado" },
+        { status: 404 }
+      );
+
+    const { seed, nonce, ...data } = game;
+
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
