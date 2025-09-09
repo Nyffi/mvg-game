0e1383036abbc8c20b9b83914376f1b95ff10c29
Paulo Rocha <pcrochadev@gmail.com>
2025-09-09 17:21:30 -0300

    fix: env fix for Vercel

--- src/app/search-params.tsx
+++ src/app/search-params.tsx
@@ -48,7 +48,7 @@ export default function SearchParams() {
         onClick={() => {
           loggedIn
             ? router.push("/blackjack")
-            : router.push(`${process.env.BACKEND_URL}/auth/google`);
+            : router.push(`${process.env.PUBLIC_BACKEND_URL}/auth/google`);
         }}
       >
         {loggedIn ? "Jogar" : "Entrar"}
